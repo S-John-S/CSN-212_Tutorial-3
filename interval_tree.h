75996079227c1aa46deef1cd17931db6e669e0ea
S-John-S <classworkpc@gmail.com>
2017-02-16 22:34:56 +0530

    Update interval_tree.h

--- interval_tree.h
+++ interval_tree.h
@@ -2,11 +2,11 @@
 #define E_INTERVAL_TREE
 
 
-#include"misc.h"
-#include"TemplateStack.H"
-#include<math.h>
-#include<limits.h>
-#include<iostream.h>
+#include "misc.h"
+#include "TemplateStack.H"
+#include <math.h>
+#include <limits.h>
+#include <iostream.h>
 
 //  The interval_tree.h and interval_tree.cc files contain code for 
 //  interval trees implemented using red-black-trees as described in 
